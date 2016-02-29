224b5d9f2b61a55f9dcccf80fdf4b28935de4a07
Karel De Coster <kareldecoster@users.noreply.github.com>
2016-02-29 15:45:08 +0100

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,22 @@
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#define NUM_POINTS 256
+#define PHI 300/76800
+#define X_0 0.75
+#define X_1 0.75
+#define X_2 2.25
+#define X_3 2.25
+#define Y_0 0.75
+#define Y_1 2.25
+#define Y_2 2.25
+#define Y_3 0.75
+
+#define AREA 0.00000702
+#define AMP_GAIN 52745.8
+#define SENSITIVITY 1.683 
+
+#define NUM_LEDS 4
+#define H 1.5
+
+#endif
